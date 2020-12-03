f5ec4cafced9641046e7042175605cd5ecf92071
Lenin Antonio Ormaza Alvarez <lenin.antonio.ormaza12@hotmail.com>
2020-12-02 21:39:38 -0500

    Las destructuraciones

---
+++ 02-fundamentos/destructuracion.js
@@ -0,0 +1,16 @@
+let deadpool = {
+    nombre: 'Wade',
+    apellido: 'Wiston',
+    poder: 'Regenración',
+    getNombre: function() {
+        return `${this.nombre} ${this.apellido} - poder: ${this.poder}`
+    }
+}
+
+/* let nombre = this.deadpool.nombre;
+let apellido = this.deadpool.apellido;
+let poder = this.deadpool.poder;
+ */
+
+let { nombre: primerNombre, apellido, poder } = deadpool;
+console.log(primerNombre, apellido, poder);
