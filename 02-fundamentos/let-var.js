88cb27297d1345e2bbad987ecbd4527b31337fd7
Lenin Antonio Ormaza Alvarez <lenin.antonio.ormaza12@hotmail.com>
2020-12-02 21:36:50 -0500

    Lets vs Var

---
+++ 02-fundamentos/let-var.js
@@ -0,0 +1,11 @@
+/* let nombre = 'Wolverin';
+if (true) {
+    let nombre = 'Magneto';
+}
+
+console.log(nombre); */
+let i = 'Hola mundo';
+for (let i = 0; i <= 5; i++) {
+    console.log(`i: ${i}`);
+}
+console.log(i);
