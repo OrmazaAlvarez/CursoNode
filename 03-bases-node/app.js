5d657ce8b1512e4570733e5a66535286c1882e77
Lenin Antonio Ormaza Alvarez <lenin.antonio.ormaza12@hotmail.com>
2020-12-07 00:12:40 -0500

    Requerir paquetes (required)

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,4 +1,14 @@
-let base = 3;
+const fs = require('fs');
+//const fs = require('express');
+//const fs = require('./fs');
+
+let base = 2;
+let data = '';
 for (let c = 1; c < 11; c++) {
-    console.log(`${base}*${c}=${base*c}`);
+    data += `${base}*${c}=${base*c}\n`;
 }
+
+fs.writeFile(`tablas/tabla-${base}.txt`, data, (err) => {
+    if (err) throw err;
+    console.log(`El archivo tabla-${base}.txt ha sido creado`);
+});
