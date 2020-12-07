992dbe5818c92a48c6e0a8a395365d0c4cf079d7
Lenin Antonio Ormaza Alvarez <lenin.antonio.ormaza12@hotmail.com>
2020-12-06 22:26:10 -0500

    Problemas comunes de los Callbacks

---
+++ 02-fundamentos/callback2.js
@@ -0,0 +1,52 @@
+let empleados = [{
+    id: 1,
+    nombre: 'Lenin'
+}, {
+    id: 2,
+    nombre: 'Ormaza'
+}, {
+    id: 3,
+    nombre: 'Alvarez'
+}];
+
+let salarios = [{
+    id: 1,
+    salario: 1000
+}, {
+    id: 2,
+    salario: 2000
+}];
+
+let getEmpleado = (id, callback) => {
+    let empleadoDB = empleados.find(empleado => empleado.id === id)
+    if (!empleadoDB) {
+        callback(`No existe un empleado con el ID ${id}`)
+    } else {
+        callback(null, empleadoDB);
+    }
+}
+
+let getSalario = (empleado, callback) => {
+    let salarioDB = salarios.find(salario => salario.id === empleado.id)
+    if (!salarioDB) {
+        callback(`No se encontró un salario para el usuario ${empleado.nombre}`)
+    } else {
+        callback(null, {
+            id: empleado.id,
+            nombre: empleado.nombre,
+            salario: salarioDB.salario
+        })
+    }
+}
+
+getEmpleado(2, (err, empleado) => {
+    if (err) {
+        return console.log(err);
+    }
+    getSalario(empleado, (err, result) => {
+        if (err) {
+            return console.log(err);
+        }
+        console.log(`El salario de ${result.nombre} es de $${result.salario}`)
+    })
+});
