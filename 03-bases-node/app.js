a17b1d7cfdcc0e3d3322eea09938948b7e926eb8
Lenin Antonio Ormaza Alvarez <lenin.antonio.ormaza12@hotmail.com>
2020-12-10 00:07:06 -0500

    Usando el paquete Yargs

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,9 +1,24 @@
+const argv = require('yargs')
+    .command('listar', 'Imprime en consola la tabla de multiplicar', {
+        base: {
+            demand: true,
+            alias: 'b'
+        },
+        limite: {
+            alias: 'l',
+            default: 10
+        }
+    })
+    .help()
+    .argv;
 const { crearArchivo } = require("./multiplicar/multiplicar");
 
-let argv = process.argv;
-let parametro = argv[2];
-let base = parametro.split('=')[1];
+let argv2 = process.argv;
+console.log(`Limite ${argv.limite}`);
+
+/* let parametro = argv[2];
+let base = parametro.split('=')[1]; 
 
 crearArchivo(base)
     .then(archivo => console.log(`Archivo creado: ${archivo}`))
-    .catch(err => console.log(err));
+    .catch(err => console.log(err)); */
