ce35202d7f7adfd2316c1c2beb545c9509358acc
Lenin Antonio Ormaza Alvarez <lenin.antonio.ormaza12@hotmail.com>
2020-12-10 21:45:55 -0500

    Optimizacion de la configuración del Yargs

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,28 +1,5 @@
-const argv = require('yargs')
-    .command('listar', 'Imprime en consola la tabla de multiplicar', {
-        base: {
-            demand: true,
-            alias: 'b'
-        },
-        limite: {
-            alias: 'l',
-            default: 10
-        }
-    })
-    .command('crear', 'Crea un archivo de texto agregando a su contenido la tabla de multimplicar', {
-        base: {
-            demand: true,
-            alias: 'b'
-        },
-        limite: {
-            alias: 'l',
-            default: 10
-        }
-    })
-    .help()
-    .argv;
+const argv = require('./config/yargs').argv;
 const { crearArchivo, listar } = require("./multiplicar/multiplicar");
-
 let comando = argv._[0];
 
 switch (comando) {
