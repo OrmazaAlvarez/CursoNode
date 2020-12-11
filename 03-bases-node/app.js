f7c668473dbf7c87d9d25c0316075d49000bc52b
Lenin Antonio Ormaza Alvarez <lenin.antonio.ormaza12@hotmail.com>
2020-12-10 21:08:57 -0500

    Ejecutar el comando: listar

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -9,16 +9,38 @@ const argv = require('yargs')
             default: 10
         }
     })
+    .command('crear', 'Crea un archivo de texto agregando a su contenido la tabla de multimplicar', {
+        base: {
+            demand: true,
+            alias: 'b'
+        },
+        limite: {
+            alias: 'l',
+            default: 10
+        }
+    })
     .help()
     .argv;
-const { crearArchivo } = require("./multiplicar/multiplicar");
+const { crearArchivo, listar } = require("./multiplicar/multiplicar");
 
-let argv2 = process.argv;
-console.log(`Limite ${argv.limite}`);
+let comando = argv._[0];
+
+switch (comando) {
+    case 'listar':
+        listar(argv.base, argv.limite)
+            .then(tabla => console.log(tabla))
+            .catch(err => console.log(err));
+        break;
+    case 'crear':
+        crearArchivo(argv.base, argv.limite)
+            .then(archivo => console.log(`Archivo creado: ${archivo}`))
+            .catch(err => console.log(err));
+        break;
+    default:
+        console.log('Comando no encontrado');
+}
+
+//console.log(argv);
 
 /* let parametro = argv[2];
-let base = parametro.split('=')[1]; 
-
-crearArchivo(base)
-    .then(archivo => console.log(`Archivo creado: ${archivo}`))
-    .catch(err => console.log(err)); */
+let base = parametro.split('=')[1]; */
--- 03-bases-node/multiplicar/multiplicar.js
+++ 03-bases-node/multiplicar/multiplicar.js
@@ -1,22 +1,36 @@
 const fs = require('fs');
+const { resolve } = require('path');
 //const fs = require('express');
 //const fs = require('./fs');
-
-let crearArchivo = (base) => {
-    return new Promise((reslve, reject) => {
+let tabla = (base, limite) => {
+    return new Promise((resolve, reject) => {
         if (Number(base)) {
             let data = '';
-            for (let c = 1; c < 11; c++) {
+            for (let c = 1; c <= limite; c++) {
                 data += `${base}*${c}=${base*c}\n`;
             }
-
-            fs.writeFile(`tablas/tabla-${base}.txt`, data, (err) => {
-                if (err) reject(err);
-                else reslve(`tabla-${base}.txt`);
-            });
+            resolve(data)
         } else {
             reject(`El valor introducido ${base} no es un número`);
         }
     });
 };
-module.exports = { crearArchivo };
+
+let listar = (base, limite) => {
+    return new Promise((resolve, reject) =>
+        tabla(base, limite)
+        .then(data => resolve(data))
+        .catch(err => reject(err)));
+};
+
+let crearArchivo = (base, limite) => {
+    return new Promise((resolve, reject) => {
+        tabla(base, limite).then(data => {
+            fs.writeFile(`tablas/tabla-${base}-all-${limite}.txt`, data, (err) => {
+                if (err) reject(err);
+                else resolve(`tabla-${base}-all-${limite}.txt`);
+            });
+        }).catch(err => reject(err));
+    });
+};
+module.exports = { crearArchivo, listar };
